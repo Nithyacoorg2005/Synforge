d6ab2d49b3503b65795ab7abf8f30cc72277dd0c
NithyashreeCS <nityaatria99@gmail.com>
2026-04-06 18:20:20 +0530

    url changed

--- synforge/frontend/src/services/api.js
+++ synforge/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Set this to your local FastAPI URL or your production URL later
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'const API_URL = process.env.REACT_APP_API_URL || "https://synforge.onrender.com"';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
